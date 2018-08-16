e91ca3182ed2330585bf7a54f948b148d649f9b6
Ruslan <nrgruslan@gmail.com>
2018-08-16 11:46:58 +0600

    DatePicker и TimePicker

--- app/src/main/java/com/example/ruslan/toast/MainActivity.java
+++ app/src/main/java/com/example/ruslan/toast/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.ruslan.toast;
 
+import android.animation.TimeAnimator;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -16,8 +19,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.date_time_layout);
 
-        setContentView(R.layout.radiobutton_layout);
+        TextView dateDefault = (TextView)findViewById(R.id.dateDefault);
+        TextView timeDefault = (TextView)findViewById(R.id.timeDefault);
+
+        DatePicker dp = (DatePicker)this.findViewById(R.id.datePicker);
+        // Месяц начиная с нуля. Для отображения добавляем 1.
+        dateDefault.setText("Дата по умолчанию " + dp.getDayOfMonth() + "/" +
+                (dp.getMonth() + 1) + "/" + dp.getYear());
+
+        dp.init(2015, 02, 01, null);
+        TimePicker tp = (TimePicker)this.findViewById(R.id.timePicker);
+        java.util.Formatter timeF = new java.util.Formatter();
+        timeF.format("Время по умолчанию %d:%02d", tp.getCurrentHour(),
+                tp.getCurrentMinute());
+        timeDefault.setText(timeF.toString());
+        tp.setIs24HourView(true);
+        tp.setCurrentHour(new Integer(10));
+        tp.setCurrentMinute(new Integer(10));
+
+
+//        setContentView(R.layout.radiobutton_layout);
 //        setContentView(R.layout.toggle_layout);
 //        setContentView(R.layout.checkbox_layout);
 //        setContentView(R.layout.activity_main);
