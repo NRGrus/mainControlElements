ab2cd6e26e2b87ebd979a89514b9d7e393d17df0
Ruslan <nrgruslan@gmail.com>
2018-08-16 11:14:09 +0600

    Checkbox

--- app/src/main/java/com/example/ruslan/toast/MainActivity.java
+++ app/src/main/java/com/example/ruslan/toast/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.checkbox_layout);
+//        setContentView(R.layout.activity_main);
     }
 
     public void onClick(View view) {
@@ -23,5 +26,39 @@ public class MainActivity extends AppCompatActivity {
 
         Snackbar.make(view, "Zhumabek Ruslan", Snackbar.LENGTH_LONG).show();
 
+    }
+
+    public void onCheckboxClicked(View view) {
+//        CheckBox language = (CheckBox) view;
+//        boolean checked = language.isChecked();
+//
+//        TextView selection = (TextView) findViewById(R.id.selection);
+////        switch (view.getId()) {
+////            case R.id.java:
+////                if (checked) {
+////                    selection.setText("Java");
+////                } break;
+////            case R.id.javascript:
+////                if (checked)
+////                    selection.setText("JavaScript");
+////                break;
+////        }
+//
+//        if (language.isChecked())
+//            selection.setText(language.getText());
+
+        CheckBox java = (CheckBox) findViewById(R.id.java);
+        CheckBox javascript = (CheckBox) findViewById(R.id.javascript);
+        TextView selection = (TextView) findViewById(R.id.selection);
+
+        String selectedItems = "";
+
+        if(java.isChecked())
+            selectedItems +=java.getText();
+        if(javascript.isChecked())
+            selectedItems +=javascript.getText();
+
+
+        selection.setText(selectedItems);
     }
 }
